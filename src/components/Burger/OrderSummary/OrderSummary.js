734932390e52f5c9ee4b6cdc97a6ddf225965751
Julien Texier <julientexier@ID22006.lan>
2020-12-16 13:03:41 +0200

    end of section 8

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+import Aux from '../../../hoc/Aux/Aux';
+import Button from '../../UI/Button/Button';
+
+class OrderSummary extends Component {
+  // (Just for debugging purposes) This should be a functional component, doesn't have to be a class based component
+  componentDidUpdate() {
+    console.log('[OrderSummary] DidUpdate')
+  }
+
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients)
+      .map(igKey => {
+        return (
+          <li key={igKey}>
+            <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {this.props.ingredients[igKey]}
+          </li>
+        );
+      })
+
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <u>
+          {ingredientSummary}
+        </u>
+        <p><strong>Total Price: {this.props.price.toFixed(2)} €</strong></p>
+        <p>Continue to checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+      </Aux >
+    );
+
+  }
+};
+
+export default OrderSummary;
